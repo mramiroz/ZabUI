4d25bb8c326b20b1f07700fb8ca0803e9bcf597f
Manuel <vassgit.vass.es:manuel.ramiro@vasscompany.com>
2024-04-15 15:58:55 +0200

    cambios

--- src/components/component/card.tsx
+++ src/components/component/card.tsx
@@ -2,7 +2,6 @@ import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 import React from 'react';
 import Link from 'next/link';
 import Copy from './copy';
-import * as Comps from '@compui/comps';
 import { ObjectId } from 'mongodb';
 
 interface CardProps {
@@ -15,11 +14,10 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ id,  code, title, description, category, props}) => {
-  const Component = Comps[category];
   return (
     <div className="rounded overflow-hidden shadow-lg m-4 flex flex-col justify-between">
       <div>
-        <Component {...props}/>
+        <h2>Componente</h2>
       </div>
       <div className="flex justify-between items-center px-6 py-4">
         <Link href={`/component/${id}`} className="inline-block bg-blue-500 rounded-full p-3 p-1 text-sm font-semibold text-gray-700 mr-2">
